889c826a1d61b0089fc717ddd76200cc469e18e1
Albertas Šarutis <aestheticrice@gmail.com>
2020-04-23 15:25:31 +0300

    Implement UserChromeCss read/write

--- lib/src/main/kotlin/dev/stashy/ffmanager/user/UserChromeCss.kt
+++ lib/src/main/kotlin/dev/stashy/ffmanager/user/UserChromeCss.kt
@@ -1,22 +1,47 @@
 package dev.stashy.ffmanager.user
 
 import dev.stashy.ffmanager.`package`.ChromePackage
+import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.streams.asSequence
 
 class UserChromeCss(var path: Path) {
-    val regex = Regex("@import [\"'](.*)[\"'];")
+    val regex = Regex("@import [\"'](.*)[\"'];", RegexOption.IGNORE_CASE)
 
-    constructor(chrome: Chrome) {
-        path = chrome.path.resolve("userChrome.css")
+    constructor(chrome: Chrome): this(chrome.path.resolve("userChrome.css"))
+
+    init {
+        read()
     }
 
-    val activePackagePaths = mutableListOf<Path>()
+    lateinit var activePackagePaths: MutableList<Path>
 
     fun add(pkg: ChromePackage) {
-
+        activePackagePaths.add(pkg.path.relativize(path.parent))
     }
 
     fun remove(pkg: ChromePackage) {
+        activePackagePaths.remove(pkg.path.relativize(path.parent))
+    }
 
+    fun read() {
+        activePackagePaths =
+            if (Files.exists(path))
+                Files.lines(path).asSequence()
+                    .map { regex.matchEntire(it) }
+                    .filter { it != null && it.groups.count() == 2 }
+                    .map { Path.of(it!!.groups[1]!!.value) }
+                    .toMutableList()
+            else
+                mutableListOf()
+    }
+
+    fun flush() {
+        if (!Files.exists(path))
+            Files.createFile(path)
+        Files.write(path,
+            activePackagePaths.map {
+                "@import \"" + it.relativize(path.parent).toString() + "\";"
+            }.toList())
     }
 }
