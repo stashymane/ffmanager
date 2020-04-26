d63584e722a6b1d6510d8d15cea16d167c332423
Albertas Šarutis <aestheticrice@gmail.com>
2020-04-26 15:17:14 +0300

    Start commandline module

---
+++ cli/src/main/kotlin/dev/stashy/ffmanager/ui/Program.kt
@@ -0,0 +1,12 @@
+package dev.stashy.ffmanager.ui
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+fun main(args: Array<String>) {
+    Program().main(args)
+}
+
+class Program : CliktCommand() {
+    override fun run() {
+    }
+}
