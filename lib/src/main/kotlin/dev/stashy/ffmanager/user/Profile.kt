95182d47e69a162c08cac152becd63f399e6f264
Albertas Šarutis <aestheticrice@gmail.com>
2020-04-26 14:12:42 +0300

    Add default profile finder

--- lib/src/main/kotlin/dev/stashy/ffmanager/user/Profile.kt
+++ lib/src/main/kotlin/dev/stashy/ffmanager/user/Profile.kt
@@ -1,5 +1,10 @@
 package dev.stashy.ffmanager.user
 
+import net.harawata.appdirs.AppDirs
+import net.harawata.appdirs.AppDirsFactory
+import java.io.File
+import java.io.FilenameFilter
+import java.lang.IllegalStateException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.stream.Collectors
@@ -8,6 +13,7 @@ class Profile(val root: Path) {
     init {
         require(isProfile(root)) { "Provided path is not a profile." }
     }
+
     val prefs: Prefs by lazy { Prefs(this) }
 
     companion object {
@@ -16,6 +22,14 @@ class Profile(val root: Path) {
             return Files.list(path).map {
                 Profile(path)
             }.collect(Collectors.toList())
+        }
+
+        val default: Profile by lazy {
+            val profiles = Path.of(AppDirsFactory.getInstance().getUserDataDir("Firefox", null, "Mozilla", true)).resolve("Profiles")
+            val path = profiles.toFile().listFiles { file: File, _: String -> file.isDirectory }?.filter {
+                it.name.contains("default-release")
+            }?.reduce { _, _ -> throw IllegalStateException("Unable to find default profile.") }?.toPath()
+            Profile(path!!)
         }
 
         fun isProfile(path: Path): Boolean {
