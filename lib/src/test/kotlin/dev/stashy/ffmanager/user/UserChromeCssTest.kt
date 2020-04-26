f5b99ad7d8736425582b00a2ad86faf3f6eeb877
Albertas Šarutis <aestheticrice@gmail.com>
2020-04-26 14:12:23 +0300

    Implement basic userchromecss tests

---
+++ lib/src/test/kotlin/dev/stashy/ffmanager/user/UserChromeCssTest.kt
@@ -0,0 +1,36 @@
+package dev.stashy.ffmanager.user
+
+import org.junit.Test
+import java.io.File
+import java.nio.file.Path
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+
+class UserChromeCssTest {
+
+    var chromePath = Path.of(Thread.currentThread().contextClassLoader.getResource("profile/chrome/userChrome.css")!!.toURI())
+
+    @Test
+    fun testRead() {
+        val chrome = UserChromeCss(chromePath)
+        chrome.activePackagePaths.apply {
+            assertEquals(2, count())
+            assertTrue(containsAll(listOf(Path.of("package1/test.css"), Path.of("package2/test.css"))))
+        }
+    }
+
+    @Test
+    fun testWrite() {
+        val file = File.createTempFile("userChrome", "css")
+        file.deleteOnExit()
+        val chrome = UserChromeCss(file.toPath())
+        val testPaths = listOf("test1", "test2").map { Path.of(it) }.toList()
+        chrome.apply {
+            activePackagePaths.addAll(testPaths)
+            flush()
+            read()
+            assertTrue(activePackagePaths.containsAll(testPaths))
+        }
+    }
+}
