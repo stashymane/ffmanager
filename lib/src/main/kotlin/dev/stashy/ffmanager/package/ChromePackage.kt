0e0c57b93a252e6ebf2ae767485e39ed266bfc0a
Albertas Šarutis <aestheticrice@gmail.com>
2020-05-07 20:47:38 +0300

    Don't use semver in package itself
    & some todos so i dont forget

--- lib/src/main/kotlin/dev/stashy/ffmanager/package/ChromePackage.kt
+++ lib/src/main/kotlin/dev/stashy/ffmanager/package/ChromePackage.kt
@@ -2,7 +2,6 @@ package dev.stashy.ffmanager.`package`
 
 import com.beust.klaxon.Json
 import com.beust.klaxon.Klaxon
-import com.github.zafarkhaja.semver.Version
 import net.lingala.zip4j.ZipFile
 import java.net.URL
 import java.nio.file.Files
@@ -14,15 +13,15 @@ data class ChromePackage(
     var path: Path? = null,
     val name: String? = null,
     val description: String? = null,
-    val version: Version? = null,
-    val compatible: List<Version> = listOf(),
+    val version: String? = null,
+    val compatible: List<String> = listOf(),
     val updateUrl: URL? = null
 ) {
 
     companion object {
         fun fromPath(path: Path): ChromePackage {
             require(Files.isDirectory(path)) { "Package path must be a directory." }
-            var p = Klaxon().parse<ChromePackage>(path.resolve("meta.json").toFile())
+            var p = Klaxon().parse<ChromePackage>(path.resolve("meta.json").toFile()) //TODO converter for URL
             if (p != null)
                 p.path = path
             else
@@ -30,7 +29,7 @@ data class ChromePackage(
             return p
         }
 
-        fun fromZip(path: Path): ChromePackage? {
+        fun fromZip(path: Path): ChromePackage? { //TODO read directly from zip
             require(path.endsWith(".zip")) { "Given path is not a zip file." }
             val tempPath = Path.of(System.getProperty("java.io.tmpdir")).resolve(path.fileName)
 
@@ -43,5 +42,4 @@ data class ChromePackage(
     override fun equals(other: Any?)
             = (other is ChromePackage)
             && id == other.id
-    // ...
 }
