8c43e9bddef086abf9e07f922488179ddff25554
Albertas Šarutis <aestheticrice@gmail.com>
2020-05-10 14:39:22 +0300

    Update chrome

--- lib/src/main/kotlin/dev/stashy/ffmanager/user/Chrome.kt
+++ lib/src/main/kotlin/dev/stashy/ffmanager/user/Chrome.kt
@@ -1,6 +1,8 @@
 package dev.stashy.ffmanager.user
 
 import dev.stashy.ffmanager.`package`.ChromePackage
+import java.lang.IllegalArgumentException
+import java.nio.file.CopyOption
 import java.nio.file.Files
 import java.nio.file.Path
 
@@ -8,26 +10,42 @@ class Chrome(var path: Path) {
 
     constructor(profile: Profile) : this(profile.root.resolve("chrome"))
 
-    val userChrome: UserChromeCss by lazy { UserChromeCss(this) }
+    private val userChrome: UserChromeCss by lazy { UserChromeCss(this) }
 
-    private val installedPackages = mutableListOf<ChromePackage>()
+    val installedPackages: List<ChromePackage>
+    get() {
+        return path.toFile().listFiles()!!.mapNotNull { if (it.isDirectory) ChromePackage.from(it) else null }
+    }
+
+    val enabledPackages: List<ChromePackage>
+    get() {
+        return userChrome.mapNotNull { try { ChromePackage.from(path.resolve(it)) } catch (e: IllegalArgumentException) { null } }
+    }
+
+    fun getInstalledPackage(pkg: ChromePackage): ChromePackage {
+        return installedPackages.find { it.id == pkg.id }
+            ?: throw NoSuchElementException("Package has not been installed.")
+    }
 
     fun enable(pkg: ChromePackage) {
-
+        userChrome.add(getInstalledPackage(pkg).path!!)
+        userChrome.flush()
     }
 
     fun disable(pkg: ChromePackage) {
-
+        userChrome.remove(getInstalledPackage(pkg).path!!)
+        userChrome.flush()
     }
 
     fun install(pkg: ChromePackage) {
         pkg.path.let {
             require(it != null && Files.isDirectory(it)) { "Package path must be a directory." }
-            Files.copy(it, path.resolve(pkg.id))
+            it.toFile().copyRecursively(path.resolve(pkg.id).toFile())
         }
     }
 
     fun uninstall(pkg: ChromePackage) {
-        installedPackages.first { it == pkg }.let { Files.delete(it.path!!) }
+        val installed = getInstalledPackage(pkg)
+        Files.delete(installed.path!!)
     }
 }
