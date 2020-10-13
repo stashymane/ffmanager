d61e66af7bd6c7dde982ff8b298bf9900a7bd37f
stashymane <aestheticrice@gmail.com>
2020-10-13 18:23:05 +0300

    Implement ChromeStyles

--- lib/src/main/kotlin/dev/stashy/ffmanager/user/ChromeStyles.kt
+++ lib/src/main/kotlin/dev/stashy/ffmanager/user/ChromeStyles.kt
@@ -1,5 +1,43 @@
 package dev.stashy.ffmanager.user
 
-class ChromeStyles {
+import dev.stashy.ffmanager.packages.FFPack
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+import kotlin.streams.toList
 
+class ChromeStyles(val path: Path, val profilePath: Path = path.parent) : ArrayList<FFPack>() {
+    constructor(chrome: Chrome) : this(chrome.path.resolve("userChrome.css"), chrome.path)
+
+    companion object {
+        val regex = Regex("@import url\\(\"(.+)\"\\);")
+    }
+
+    init {
+        if (!Files.exists(path))
+            Files.createFile(path)
+        load()
+    }
+
+    fun load() {
+        clear()
+        addAll(Files.lines(path)
+            .map { regex.matchEntire(it) }
+            .filter { it != null }
+            .map { it!!.groupValues[0] }
+            .map { Paths.get(it) }
+            .map { it.parent }
+            .map { FFPack.from(it)!! }
+            .toList()
+        )
+    }
+
+    fun save() {
+        Files.write(
+            path,
+            this.map { it.id }.map { "$it/userChrome.css" }.toList(),
+            StandardOpenOption.TRUNCATE_EXISTING
+        )
+    }
 }
