e35bcd1528619e80f29cc02934624a56b0fa8d5b
stashymane <aestheticrice@gmail.com>
2020-10-13 14:46:34 +0300

    Refactor chrome

--- lib/src/main/kotlin/dev/stashy/ffmanager/user/Chrome.kt
+++ lib/src/main/kotlin/dev/stashy/ffmanager/user/Chrome.kt
@@ -1,59 +1,49 @@
 package dev.stashy.ffmanager.user
 
+import dev.stashy.ffmanager.FFUtil
 import dev.stashy.ffmanager.packages.FFPack
 import dev.stashy.ffmanager.packages.PackFiles
 import java.nio.file.Files
 import java.nio.file.Path
-import kotlin.streams.toList
+import java.nio.file.Paths
+import kotlin.streams.asSequence
 
-class Chrome(val path: Path) :
-    ArrayList<FFPack>() {
-    constructor(profile: Profile) : this(profile.path.resolve("meta.json"))
-
-    val styles = ChromeStyles()
-    val scripts = ChromeScripts()
+class Chrome(val path: Path) {
+    constructor(profile: Profile) : this(profile.path.resolve("chrome"))
 
     init {
         if (!Files.exists(path))
             Files.createDirectory(path)
-        reload()
     }
 
-    override fun add(pack: FFPack): Boolean {
-        if (contains(pack))
-            throw AlreadyInstalledException()
-        if (pack !is PackFiles)
-            return false
-        val ipath = path.resolve(pack.id)
-        Files.copy(pack.path, ipath)
-        val installed = FFPack.from(ipath)
-        enable(installed)
-        return super.add(installed)
+    val styles = ChromeStyles(this)
+    val scripts = ChromeScripts(this)
+
+    val installed
+        get() = Files.list(path).asSequence().mapNotNull { FFPack.from(it) }
+
+    fun isInstalled(pack: FFPack): Boolean {
+        val p = Paths.get(pack.id)
+        return Files.list(path).anyMatch { it.fileName == p }
     }
 
-    override fun remove(pack: FFPack): Boolean {
-        if (!contains(pack)) return false
-        if (pack !is PackFiles)
-            return false
-        disable(pack)
-        Files.delete(pack.path)
-        return super.remove(pack)
+    fun install(pack: PackFiles) {
+        if (isInstalled(pack))
+            throw AlreadyInstalledException(pack)
+        FFUtil.copyAll(pack.path, path.resolve(pack.id))
     }
 
-    fun enable(pack: FFPack) {
-
+    fun uninstall(id: String) {
+        val p = Paths.get(id)
+        Files.list(path).asSequence().find { it.fileName == p }?.let {
+            FFUtil.deleteAll(it)
+        }
     }
 
-    fun disable(pack: FFPack) {
-
+    fun uninstall(pack: FFPack) {
+        uninstall(pack.id)
     }
 
-    fun reload() {
-        clear()
-        addAll(Files.list(path).filter { Files.isDirectory(it) }.map {
-            FFPack.from(it)
-        }.toList())
-    }
 }
 
-class AlreadyInstalledException : Exception()
+class AlreadyInstalledException(pkg: FFPack) : Exception("Package ${pkg.id} has already been installed.")
